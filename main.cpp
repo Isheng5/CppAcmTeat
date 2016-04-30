422a51be3bd4c498e12daccc84af63f0906de8fd
isheng <zengjiean@huas.edu.gr>
2016-04-30 15:17:45 +0800

    Update .gitignore

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-int acm2017();
+int acm2016();
 
 int main(int argc, char const *argv[])
 {
-    acm2017();//通过
+    acm2016();//通过
 
     cout << "通过" << endl;
     return 0;
