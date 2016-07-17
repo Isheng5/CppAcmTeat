af226a48b0c79d7857edd7ccc2364eaffdd227c6
isheng52 <zengjiean@gmail.com>
2016-07-17 20:38:22 +0800

    第二周A题，通过

--- huas/week2/week2_text_A.cpp
+++ huas/week2/week2_text_A.cpp
@@ -12,7 +12,7 @@ using namespace std;
 int Week2TextA[w2max],WeekText5[w2max];//建节点统计表。
 
 void w2ta_star(){
-    for (int i = 0; i < w2max; ++i) {
+    for (int i = 0; i < w2max; ++i) {//初始化数据
         Week2TextA[i] = 0;
         WeekText5[i] = 0;
     }
@@ -33,26 +33,26 @@ int week2_text_a(){
         while(scanf("%d %d",&a,&b)){
             if(a == 0 || b == 0) break;
             if(a == -1 || b == -1) break;
-            Week2TextA[b] ++;
-            WeekText5[a] ++;
-            num ++;
+            Week2TextA[b] ++;//统计节点数量
+            WeekText5[a] ++;//统计节点数量
+            num ++;//统计边的数量
         }
         if(a == -1 || b == -1) break;
-        for (int i = 1; i < w2max; ++i) {
-            if(Week2TextA[i] > 1) {
+        for (int i = 1; i < w2max; ++i) {//处理的数量
+            if(Week2TextA[i] > 1) {//判断是否每个节点只有一条有向边指向它
                 ok = 0;
                 break;
             }
             else if(WeekText5[i] > 0 || Week2TextA[i] > 0) map ++;
             else continue;
         }
-        if(num == map - 1 ){
+        if(num == map - 1 ){//根据离散数学公式：节点数量 = 边数 - 1 判断该图为树
             ok = 1;
         }
-        if(num == 0) ok = 1;
+        if(num == 0) ok = 1;//排除空树特殊情况
         if(ok == 1) printf("Case %d is a tree.\n",n);
         else printf("Case %d is not a tree.\n",n);
-        if(a == -1 || b == -1) break;
+        if(a == -1 || b == -1) break;//程序终止的条件
     }
     return 0;
 }
